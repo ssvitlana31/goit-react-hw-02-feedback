aab64e6ee0032a07c8ed79a1794e092ee8fe7604
ssvitlana31 <130747746+ssvitlana31@users.noreply.github.com>
2023-10-21 08:18:51 +0300

    stat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+}
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Statistics = ({ good, neutral, bad, total, positiveFeedback }) => {
+  return (
+    <>
+      <h2>Statiatics</h2>
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>Total: {total}</li>
+        <li>Positive Feedback: {positiveFeedback}%</li>
+      </ul>
+    </>
+  );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  total: PropTypes.number,
+  positiveFeedback: PropTypes.number,
+};
